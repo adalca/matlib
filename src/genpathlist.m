8e9a10011ec8692e8e865eed5166c498ccefe2e5
Adrian Dalca <adalca@mit.edu>
2014-01-12 17:05:13 -0500

    Update genpathlist.m doc

--- src/genpathlist.m
+++ src/genpathlist.m
@@ -1,8 +1,16 @@
 function genpaths = genpathlist(paths)
-% run genpath on all paths inside the given path list
-%	path list should be in format /path/1;/path/2/etc;/and/so/on
-    
+% GENPATHLIST run genpath on a path string 
+% genpaths = genpathlist(paths) run genpath() on all paths inside the given path string
+%	path string list should be in format /path/1;/path/2/etc;/and/so/on
+%
+% See Also: genpath
+%
+% Contact http://adalca.mit.edu
+
+    % get the different paths in the path string
     s = strsplit(paths, ';');
+    
+    % compute the genpath for each one and combine
     genpaths = '';
     for i = 1:numel(s)
         genpaths = [genpaths, genpath(s{i})];
