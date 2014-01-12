d8bf5ba790250cc8b363e070787e0ff852304e3f
Adrian Dalca <adalca@mit.edu>
2014-01-12 14:35:42 -0500

    Update shiftElements.m
    updated commends and description.

--- src/shiftElements.m
+++ src/shiftElements.m
@@ -1,17 +1,31 @@
 function vec = shiftElements(vec, oldIdx, newIdx)
-% shiftElements in vector v according to a index permutation
-%   e.g. v = [1 2 3 4 5 6 7], oldIdx = 5, newIdx = 2
+% SHIFTELEMENTS shift elements in given vector
+% vec = shiftElements(vec, oldIdx, newIdx) shift elements in vector vec according 
+%   to a index permutation given by oldIdx (the index of the element to be moved)
+%   and newIdx (the index where the element should be inserted). The other elements
+%   will shift around to allow for this change
+%
+% Example: put the fifth element in the second position, move everything between 
+%   second:fourth position to third:fifth.
+%   v = [1 2 3 4 5 6 7], oldIdx = 5, newIdx = 2
+%   v = shiftElements(v, oldIdx, newIdx)
 %   output: v = [1 5 2 3 4 6 7]
+%
+% Contact: adalca.mit.edu
     
+    % get length of vector
     len = length(vec);
 
+    % compute the index array
     if newIdx > oldIdx
         idx = [1:(oldIdx-1), (oldIdx+1):newIdx, oldIdx, (newIdx + 1):len];
     elseif newIdx < oldIdx
         idx = [1:(newIdx-1), oldIdx, (newIdx):(oldIdx-1), (oldIdx+1):len];
     end
     
+    % verify dimensions
     assert(numel(idx) == numel(vec));
     
+    % make the modification
     vec = vec(idx);
     
