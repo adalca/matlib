dc813be94e14b218bea16fad012caaa427f1d535
Adrian Dalca <adalca@mit.edu>
2014-01-12 16:46:52 -0500

    fulldir.m: more robust and more similar to dir

--- src/fulldir.m
+++ src/fulldir.m
@@ -1,12 +1,35 @@
 function d = fulldir(path)
-% acts the same as dir, but includes full path for files
-% not tested very thoroughly yet.
+% FULLDIR get files and folder with pull path
+%   d = fulldir() get files and folders in current path. Acts the same
+%   as d=dir(), but includes full path for files in d.name.
+%
+%   d = fulldir(path) allows for specification of the path to list
+%
+% See Also: dir
+%
+% Contact: http://adalca.mit.edu
 
-    [ppath, ~, ~] = fileparts(path);
-
-    d = dir(path);
     
+    % if no argument given, assume present workding directory
+    if nargin == 0
+        path = pwd;
+    end
+    
+    % get the folder path name
+    [ppath, ~, ~] = fileparts(path);
+    
+    % get the files
+    d = dir(path);
+
+    % combine the name of each file with path name
     for e = 1:numel(d)
         d(e).name = fullfile(ppath, d(e).name);
+    end
+    
+    % if no output argument, just list names
+    if nargout == 0
+        for e = 1:numel(d)
+            fprintf(d(e).name);
+        end
     end
     
