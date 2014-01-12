b9b2b87b28f064b3e9ce0435beb2888287a39976
Adrian Dalca <adalca@mit.edu>
2014-01-12 14:41:30 -0500

    Update amParallel.m

--- src/amParallel.m
+++ src/amParallel.m
@@ -1,2 +1,9 @@
 function am = amParallel()
+% AMPARALLEL true iff parallel pool is open
+%   am = amParallel() returns true if and only if parallel pool is open
+%
+% See Also: matlabpool
+%
+% Contact: adalca.mit.edu
+
     am = matlabpool('size') == 0;
