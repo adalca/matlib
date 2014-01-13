a3def76cfc0a1d19c2fd7af52ad7118c4a640c23
Adrian Dalca <adalca@mit.edu>
2014-01-12 22:33:13 -0500

    Update iseven.m

--- src/iseven.m
+++ src/iseven.m
@@ -1,4 +1,11 @@
 function b = iseven(x)
+% ISEVEN true if value is even
+%   b = iseven(x) returns true if x is even
+%
+% See Also: mod
+%
+% Contact: http://adalca.mit.edu
+
     b = mod(x, 2) == 0;
     
 
