09b58bb155e128a2e740963d1c3cf21614e5da75
Adrian Dalca <adalca@mit.edu>
2014-01-12 21:44:03 -0500

    Add isIntegerValue.m doc

--- src/isIntegerValue.m
+++ src/isIntegerValue.m
@@ -1,2 +1,12 @@
 function is = isIntegerValue(x)
+% ISINTEGERVALUE check if variable has integer value
+%   is = isIntegerValue(x) will check if x has integer value. 
+%       Note, this is different from MATLAB's isinteger, which checks 
+%       the class of the variable x against Integer Types (uint8, etc)
+%
+% See Also isinteger
+%
+% Contact: http://adalca.mit.edu
+
     is = mod(x, 1) == 0;
+    
