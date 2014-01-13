e847572685c4c708a1f7c2926f8c2b7cc62e2bb2
Adrian Dalca <adalca@mit.edu>
2014-01-12 21:56:11 -0500

    Update isfieldVerbose.m

--- src/isfieldVerbose.m
+++ src/isfieldVerbose.m
@@ -1,4 +1,19 @@
 function isp = isfieldVerbose(params, field, usageMsg)
+% ISFIELDVERBOSE do isfieldcheck and complain if false
+%   isp = isfieldVerbose(params, field, usageMsg) perform a isfield(params, field) 
+%       and if it's false, print out a message according to useageMsg to stderr
+%
+% Example:
+%   >> x = struct();
+%   >> is = isfieldVerbose(x, 'notAField', ':(')
+%   Field Missing: notAField. :(
+%   is =
+%        0
+%
+% See Also: isfield
+%
+% Contact: http://adalca.mit.edu
+
     isp = isfield(params, field);
     if ~isp
         fprintf(2, 'Field Missing: %s. %s', field, usageMsg);
