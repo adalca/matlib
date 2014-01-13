26d5844f220721ae7abfb6912ef208d8eba11722
Adrian Dalca <adalca@mit.edu>
2014-01-12 21:46:56 -0500

    Update isPublishMode.m

--- src/isPublishMode.m
+++ src/isPublishMode.m
@@ -1,4 +1,10 @@
 function publishMode = isPublishMode()
+% ISPUBLISHMODE checklogical if running in publish mode
+%   publishMode = isPublishMode() logical if currently running in publish mode
+%
+% See Also: publish
+%
+% Contact: http://adalca.mit.edu
 
     % check if we're in publishing mode
     st = dbstack;
