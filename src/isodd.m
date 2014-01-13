f93a125e07c21edcf9434442f2283b6b2cd1e09d
Adrian Dalca <adalca@mit.edu>
2014-01-12 22:32:37 -0500

    Update isodd.m

--- src/isodd.m
+++ src/isodd.m
@@ -1,4 +1,13 @@
 function b = isodd(x)
+% ISODD true if value is odd
+%   b = isodd(x) returns true if x is of
+%
+% See Also: mod
+%
+% Contact: http://adalca.mit.edu
+
+
+
     b = mod(x, 2) == 1;
     
 
