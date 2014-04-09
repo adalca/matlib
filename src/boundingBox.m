c791c09b5cebe29abb418dba856987132e81bae0
adalca <adalca@mit.edu>
2014-04-09 11:10:13 -0400

    correcting boundingBox.m

--- src/boundingBox.m
+++ src/boundingBox.m
@@ -1,26 +1,39 @@
-function [boundingBox, varargout] = boundingBox(bwimage)
-%   [boundingBox, boxRange, croppedBWImage] = boundingBox(bwimage)
+function [croppedVol, cropMask, cropArray, bBox] = boundingBox(vol)
+%   [croppedVol, cropMask, cropArray, bBox] = boundingBox(vol) compute the bounding box of the 
+%       given label map
 %
-%       boundingBox as returned from regionprops
-%       boxRange is a cell array of size nDims x 1, each entry being the crop range in that
+%       croppedVol image cropped by the bounding box.
+%       cropMask bounding box mask
+%       cropArray is a cell array of size nDims x 1, each entry being the crop range in that
 %           dimension.
-%       croppedBWImage image cropped by the bounding box.
+%       bBox as returned from regionprops
+%
+%   Contact: adalca@mit.edu
 
-    nDims = ndims(bwimage);
+    nDims = ndims(vol);
+    
+    % prepare structure for regionprops to get the boundingBox
+    bwstruct.Connectivity = 3^nDims - 1;
+    bwstruct.ImageSize = size(vol);
+    bwstruct.NumObjects = 1;
+    bwstruct.PixelIdxList = {find(vol > 0)};
     
     % use regionprops to get the boundingBox
-    bwstruct.Connectivity = 3^nDims - 1;
-    bwstruct.ImageSize = size(bwimage);
-    bwstruct.NumObjects = 1;
-    bwstruct.PixelIdxList = {find(bwimage > 0)};
-    
     stats = regionprops(bwstruct, 'BoundingBox');
-    boundingBox = stats.BoundingBox;
+    bBox = stats.BoundingBox;
+    clear stats;
     
+    % switch x and y values - here matlab puts second dimension first in the style of image
+    % treatments, where it puts 'x', which is the second dimension, first.
+    [bBox(1), bBox(2)] = switchValues(bBox(1), bBox(2));
+    [bBox(nDims + 1), bBox(nDims + 2)] = switchValues(bBox(nDims + 1), bBox(nDims + 2));
+
     % crop the image  
-    if nargout > 1 
-        rangeStart = floor(stats.BoundingBox(1:nDims));
-        rangeWidth = ceil(stats.BoundingBox(nDims+1:end));
-        [varargout{2}, varargout{1}] = cropVolume(bwimage, rangeStart, rangeStart + rangeWidth);
-    end
-    
+    rangeStart = floor(bBox(1:nDims));
+    rangeWidth = ceil(bBox(nDims+1:end));
+    [croppedVol, cropArray] = cropVolume(vol, rangeStart, rangeStart + rangeWidth);
+
+    % get crop mask
+    v = false(size(vol));
+    v(cropArray{:}) = true;
+    cropMask = v;
