4a69f381b71a46e5845c9c67c80f7029b9b96d03
adalca <adalca@mit.edu>
2014-05-28 14:50:16 -0400

    significantly improve view3Dopt.m

--- src/view3Dopt.m
+++ src/view3Dopt.m
@@ -2,17 +2,115 @@ function view3Dopt(varargin)
 % VIEW3DOPT extends/wraps the view3D script with some useful options
 %	view3Dopt(X) or view3Dopt(X, 'link') or view3Dopt(X, F) will simply call view3D 
 %		with those arguments
+%
+%   view3Dopt(V1, V2, ...) displays the given volumes with default tiling and link parameters
+%
+%   view3Dopt(C) where C = {V1, V2, ...} displays the given volume with the default tiling and link
+%       parameters
+%
 %	view3Dopt(..., ParamName, ParamValue) add functioanlity as shown below
 %
 %   ParamName/ParamValue pairs:
-%       tilehorz:boolean, (default:false). if true, will tile multiple viewers horizontally on screen
-%       link:boolean (default:true). if true, will link viewers.
-%       tilefull:boolean, (default:true). tile (up to 6) on one monitor
-%           TODO: Multi-Screen Tile
+%       tilehorz:boolean (default:false). if true, tile multiple viewers horizontally on screen
+%       link:boolean     (default:true). if true, will link viewers.
+%       tilefull:boolean (default:true). tile on one monitor in a semi-smart arrangement
+%       monitorId:int    (default:1) the monitor number on which to show these volumes.
 %       voxMask:mask, (default:none). if true, then the inputs are assumed to be given as 
-%           just voxels within some mask
+%           just voxels within some mask. so v1full = mark(V1) gives an actual 3D volume.
 %
-% requires: view3D script, ifelse
+% requires: view3D script (external), ifelse, maskvox2vol if using voxMask
+%
+% See Also: view3D
+%
+% Contact: adalca@csail.mit.edu
+
+    % parse inputs to extract volumes and options
+    [vols, inputs] = parseinputs(varargin{:});
+    
+    % if given voxMask, then the inputs are assumed to be given as just voxels within that mask, so
+    % transform into volumes via maskvox2vol.
+    if ~isempty(inputs.voxMask) && numel(inputs.voxMask) > 1
+        for i = 1:numel(vols)
+            vols{i} = maskvox2vol(vols{i}, inputs.voxMask);
+        end
+    end
+        
+    % display the volumes
+    if inputs.link
+        h = view3D(vols, 'link');
+    else
+        h = view3D(vols);
+    end
+    nViews = numel(h);
+
+    % tile if necessary
+    if inputs.tilehorz || inputs.tilefull
+        
+        % get tile information (nRows is 1 if tilehorz, empty otherwise)
+        nRows = ifelse(inputs.tilehorz, {1}, {});
+        tile = tileGrid(nViews, inputs.monitorId, nRows{:});
+        
+        % get tile grid 
+        [nfo, tile] = tileInfo(nViews, tile);
+        
+        % move viewers around.
+        for i = 1:nViews
+            set(h(i), 'Position', [nfo(i).x, nfo(i).y, tile.winwidth, tile.winheight]);
+        end
+    end
+    
+end
+
+function [nfo, tile] = tileInfo(nViews, tile)
+    
+    if nargin == 1
+        tile = tileGrid(nViews, 1);
+    end
+    
+    % go through the windows.
+    nfo(nViews) = struct('x', [], 'y', []);
+    for i = 1:nViews
+        % compute the x location
+        xi = mod(i - 1, tile.nCols) + 1;
+        nfo(i).x = tile.xStart - 1 + (xi - 1) * tile.winwidth + xi;
+        
+        % compute the y location
+        yi = ceil(i ./ tile.nCols);
+        nfo(i).y = tile.yStart - 1 + tile.height - yi * (tile.titlebarheight + tile.winheight);
+    end
+end
+
+function tile = tileGrid(nViews, monitorID, nRows)
+    % TODO: take in several monitors at once. Then, simple compute the winwidth and winheight per
+    % monitor. (to not have windows across monitors. THen you have to smartly iterate
+    
+    % estimate a height for the titlebar which should be accounted for in the computations. 
+    tile.titlebarheight = 60;
+    
+    % get the size of the screen
+    % for a single screen, screensize = get(0,'ScreenSize'); would work
+    % but we want to support multiple screens. 
+    mp = get(0, 'MonitorPositions');
+    screensize = mp(monitorID, :);
+    tile.xStart = screensize(1);
+    tile.yStart = screensize(2) + (mp(1, 4) - screensize(4));
+    tile.width = screensize(3) - tile.xStart + 1;
+    tile.height = screensize(4) - tile.yStart + 1;
+
+    % compute the number of columns and rows.
+    if nargin == 2
+        tile.nRows = max(round(sqrt(nViews .* tile.height ./ tile.width)), 1);
+    else
+        tile.nRows = nRows;
+    end
+    tile.nCols = ceil(nViews ./ tile.nRows);
+    
+    % compute the width and height of the windows. 
+    tile.winwidth = tile.width / tile.nCols;
+    tile.winheight = (tile.height - tile.nRows * tile.titlebarheight) / tile.nRows;    
+end
+
+function [vols, inputs] = parseinputs(varargin)
 
     % determine the number of inputs for view3D
     f = find(cellfun(@ischar, varargin(2:end)), 1, 'first');
@@ -37,68 +135,32 @@ function view3Dopt(varargin)
     p.addParamValue('link', true, @islogical);
     p.addParamValue('tilehorz', false, @islogical);
     p.addParamValue('tilefull', true, @islogical);
-    p.addParamValue('voxMask', false, @islogical);  % requies maskvox2vol
+    p.addParamValue('monitorId', 1, @isscalar);
+    p.addParamValue('voxMask', false, @islogical);  % requies maskvox2vol()
     p.parse(extraOpts{:});
 
-    vols = varargin{1};
+    % save input options
+    inputs = p.Results;
+    
+    % if tilehorz is true, then check that 
+    if inputs.tilehorz
+        if inputs.tilefull
+            assert(any(strcmp(p.UsingDefaults, 'tilefull')), ...
+                'Please only supply horizontal or full tile, not both');
+            inputs.tilefull = false;
+        end
+    end
+    
+    % extract volumes.
+    if numel(f) == 0 && isnumeric(varargin{1})
+        vols = varargin;
+    else
+        vols = varargin(1:f);
+    end
+    
+    % make sure the volumes are in a cell
     if ~iscell(vols)
         vols = {vols};
     end
     
-    % if given voxMask, then the inputs are assumed to be given as just voxels within that mask, so
-    % transform into volumes via maskvox2vol.
-    if ~isempty(p.Results.voxMask) && numel(p.Results.voxMask) > 1
-        for i = 1:numel(vols)
-            vols{i} = maskvox2vol(vols{i}, p.Results.voxMask);
-        end
-    end
-        
-    
-    
-    % display the volumes
-    if p.Results.link
-        h = view3D(vols, 'link');
-    else
-        h = view3D(vols);
-    end
-
-    % tile horizontally if necessary
-    if p.Results.tilehorz
-        lastx = 0;
-        for i = 1:numel(h)
-            if i == 1
-                pos = get(h(i), 'Position');
-            else
-                pos = get(h(i), 'Position');
-                pos(1) = lastx + pos(3);
-            end
-
-            set(h(i), 'Position', pos);
-            lastx = pos(1);
-        end
-    end
-    
-    % tile horizontally if necessary
-    if p.Results.tilefull
-        
-        % heuristic... should imrove this. assume no more than 6 images
-        if numel(h) > 6, error('need to work on more than 6 fulltile...'); end
-        
-        titlebarheight = 60;
-        screensize = get(0,'ScreenSize');
-        winwidth = screensize(3)/3;
-        winheight = (screensize(4)-2*titlebarheight)/2; 
-        
-        for i = 1:numel(h)
-
-            xi = mod(i - 1, 3) + 1;
-            xpos = (xi - 1) * winwidth + xi;
-            yi = ifelse(i <= 3, 1, 2);
-            ypos = screensize(4) - yi * (titlebarheight + winheight);
-            
-            set(h(i), 'Position', [xpos, ypos, winwidth, winheight]);
-
-        end
-    end
-       
 end
