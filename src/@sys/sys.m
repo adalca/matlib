a3818761b2bafb02286f86af5fb54aa113fecde6
Adrian Dalca <adalca@mit.edu>
2014-05-31 13:29:55 -0400

    fixed a bug and improving support for system functions

---
+++ src/@sys/sys.m
@@ -0,0 +1,12 @@
+classdef sys < handle
+    % SYS A helper class for helpful system calls
+    
+    properties (Constant)
+    end
+    
+    methods (Static)
+        am = amParallel()
+    end
+    
+end
+
