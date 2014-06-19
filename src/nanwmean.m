a041a09a9201bc990395642e3bd3c260365578e5
Adrian Dalca <adalca@mit.edu>
2014-06-19 18:44:13 -0400

    adding nanssd.m and nanwmean.m for nan support for those functions

---
+++ src/nanwmean.m
@@ -0,0 +1,25 @@
+function y = nanwmean(x, w, dim)
+% weighted mean
+
+    narginchk(1, 3);
+    if nargin == 1 || isempty(w)
+        w = ones(size(x));
+    end
+
+    if ~exist('dim', 'var') || isempty(dim)
+        dim = find(size(x) ~= 1, 1);
+        
+        if isempty(dim), 
+            dim = 1; 
+        end
+    end
+    
+    nans = isnan(x .* w);
+    x(nans) = 0;
+    w(nans) = 0;
+    
+    s = sum(x .* w, dim);
+    z = sum(w, dim);
+    z(z == 0) = NaN;
+    y = s ./ z;
+    
