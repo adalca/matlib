1fcd3623a0304b8fcc4f9abfc79f32846b77e4cd
Adrian Dalca <adalca@mit.edu>
2014-10-24 19:01:00 -0400

    adding complexity to dispDashedLine.m

--- src/dispDashedLine.m
+++ src/dispDashedLine.m
@@ -1,4 +1,4 @@
-function dispDashedLine(fid)
+function dispDashedLine(fid, cRepeat)
 % DISPDASHEDLINE prints 80 dash characters and newline
 %   dispDashedLine(fid) prints a 80 dashed-characters newline to the given file 
 %   identifier
@@ -14,9 +14,17 @@ function dispDashedLine(fid)
     if nargin == 0
         fid = 1;
     end
+    if nargin < 2
+        cRepeat = 1;
+    end
+    
+    line = ['-----------------------------------------------', ...
+        '---------------------------------\n'];
+    if cRepeat > 1
+        line = repmat(line, [1, cRepeat]);
+    end
     
     % Note: this seems faster than repmat, etc.
-    fprintf(fid, ['-----------------------------------------------', ...
-        '---------------------------------\n']);
+    fprintf(fid, line);
     
     
