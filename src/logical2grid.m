890be0f8b2d6cfdd7cf32ab73a07b87b123f0988
adalca <adalca@mit.edu>
2014-11-14 10:27:09 -0500

    small changes in logical2grid.

--- src/logical2grid.m
+++ src/logical2grid.m
@@ -1,9 +1,23 @@
-function [grididx, gridsize, gridsubsep] = logical2grid(logicalvol)
+function [grididx, gridsize, gridsub] = logical2grid(logicalvol)
+% LOGICAL2GRID identify a grid in a logical volume
+%   [grididx, gridsize, gridsubsep] = logical2grid(logicalvol) identify a grid in a logical volume.
+%   returns grididx: the indexes of the grid in the volumes, gridsize: the size of the grid points,
+%   and gridsub: the grid subscripts.
+%
+% Example:
+%   v = logical([0 1 0 1; 0 0 0 0; 0 1 0 1])
+%   [grididx, gridsize, gridsub] = logical2grid(v)
+%   grididx is then [4, 10; 6 12];
+%   gridsize is [2, 2];
+%   gridsub is {[], []}
+%
+%
+% Author: adalca@mit
     
     nDims = ndims(logicalvol);
     
     gridsubsep = cell(nDims, 1);
-    gridsize = zeros(nDims, 1);
+    gridsize = zeros(1, nDims);
     for d = 1:nDims
         fullrange = getNdRange(size(logicalvol));
         for i = 1:size(logicalvol, d)
