566953b11495e4203e8c5e59d6b7ce893c350e4b
adalca <adalca@mit.edu>
2015-06-15 11:55:41 -0400

    introducing view2D for multiview

---
+++ src/view2D.m
@@ -0,0 +1,47 @@
+function view2D(images, varargin)
+
+    [images, nRows, nCols, inputs] = parseinputs(images, varargin{:});
+
+    figuresc()
+    for i = 1:numel(images)
+        subplot(nRows, nCols, i);
+        imagesc(images{i});
+        caxis(inputs.caxis);
+        axis off;
+        axis equal;
+    end
+
+end
+
+
+function [images, nRows, nCols, inputs] = parseinputs(images, varargin)
+
+    p = inputParser();
+    p.addRequired('images', @(x) iscell(x) || isnumeric(x));
+    p.addParameter('subgrid', -1, @isnumeric)
+    p.addParameter('caxis', [0, 1]);
+    p.parse(images, varargin{:});
+    
+    if isnumeric(images)
+        images = {images};
+    end
+    
+    if isscalar(p.Results.subgrid) && p.Results.subgrid == -1
+        [nRows, nCols] = subgrid(numel(images));
+    else
+        nRows = p.Results.subgrid(1);
+        nCols = p.Results.subgrid(2);
+    end
+    
+    inputs = p.Results;
+end
+
+function [hei, len] = subgrid(N)
+
+    screensize = get(0, 'Screensize');
+    W = screensize(3);
+    H = screensize(4);
+
+    hei = ceil(sqrt(N*H/W));
+    len = ceil(N/hei);
+end
