4f382b85117dd3c929360bec9f2ab3345989ed00
adalca <adalca@mit.edu>
2015-08-13 22:33:22 -0400

    adding assert to subvec2ind

--- src/subvec2ind.m
+++ src/subvec2ind.m
@@ -1,4 +1,5 @@
 function idx = subvec2ind(sz, sv)
+    assert(all(all(bsxfun(@ge, sz, sv))), 'Subscript outside size range');
     svc = mat2cell(sv, size(sv, 1), ones(1, size(sv, 2)));
     idx = sub2indfast(sz, svc{:});
     
