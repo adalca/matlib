bd71aeb28c8dbc47b9d553d670ad9af70776273f
adalca <adalca@mit.edu>
2015-12-15 12:42:08 -0500

    additional assert for checking ind range

--- src/ind2subvec.m
+++ src/ind2subvec.m
@@ -1,6 +1,7 @@
 function sub = ind2subvec(sz, ind)
     assert(size(ind, 2) == 1, 'need vertical vector');
-
+    assert(max(ind(:)) <= prod(sz), 'ind outside range');
+    
     sub = cell(1, numel(sz));
     [sub{:}] = ind2sub(sz, ind(:));
     sub = [sub{:}];
