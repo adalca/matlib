45f240e7380601d7c04a2a28941842c8ef7f2ec9
adalca <adalca@mit.edu>
2016-02-17 17:51:28 -0500

    add dimension support

--- src/msd.m
+++ src/msd.m
@@ -1,6 +1,14 @@
-function dst = msd(v1, v2)
+function dst = msd(v1, v2, dim)
 % mean squared difference
+%
+% if dim is supplied, it takes msd along that dim. 
+% otherwise, it takes ssd accross entire vectorized volumes
 
     ds = (v1 - v2) .^ 2;
-    dst = mean(ds, 2);
-    
+
+    if exist('dim', 'var')
+        dst = mean(ds, dim);
+    else
+        dst = mean(ds(:));
+    end
+
--- src/ssd.m
+++ src/ssd.m
@@ -1,6 +1,14 @@
-function dst = ssd(v1, v2)
+function dst = ssd(v1, v2, dim)
 % sum of squared difference
+%
+% if dim is supplied, it takes ssd along that dim. 
+% otherwise, it takes ssd accross entire vectorized volumes
 
     ds = (v1 - v2) .^ 2;
-    dst = sum(ds, 2);
+    
+    if exist('dim', 'var')
+        dst = sum(ds, dim);
+    else
+        dst = sum(ds(:));
+    end
     
